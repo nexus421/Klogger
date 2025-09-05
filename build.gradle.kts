4040ff784e1c2c4bad7229f613cec8ed59c8611e
marvin <github@kickner.bayern>
2025-09-05 15:31:09 +0200

    Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     kotlin("jvm") version "2.2.10"
+    `maven-publish`
 }
 
-group = "bayern.kickner"
+group = "com.github.nexus421"
 version = "0.0.1"
-
+val globalVersion = version.toString()
 repositories {
     mavenCentral()
 }
@@ -18,4 +19,16 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+
+            groupId = "com.github.nexus421"
+            artifactId = "Klogger"
+            version = globalVersion
+            from(components["java"])
+        }
+    }
 }
